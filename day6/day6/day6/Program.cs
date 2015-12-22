993a0178764044e38c6bbdd426005b0bc9651277
Troels Kroegh <troels.kroegh@gmail.com>
2015-12-22 22:05:29 +0100

    day6 (pt 1 + 2) + day7 (pt 1 + 2)

---
+++ day6/day6/day6/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace day6
+{
+    public class Point
+    {
+        public Point(int _x, int _y)
+        {
+            x = _x;
+            y = _y;
+            val = 0;
+        }
+
+        public int x;
+        public int y;
+        public int val { get; set; }
+    }
+
+    class Program
+    {
+        public static List<Point> points = new List<Point>();
+
+        static int PartOne()
+        {
+
+            foreach (string line in File.ReadLines("./input"))
+            {
+                // Console.WriteLine("{0}", line);
+
+                var line_content = line.Split(' ');
+                if (line_content[0] == "turn")
+                {
+                    var onOff = line_content[1] == "on";
+                    var x_min = int.Parse(line_content[2].Split(',')[0]);
+                    var y_min = int.Parse(line_content[2].Split(',')[1]);
+
+                    var x_max = int.Parse(line_content[4].Split(',')[0]);
+                    var y_max = int.Parse(line_content[4].Split(',')[1]);
+
+                    foreach (var p in points.Where(a => a.x >= x_min && a.y >= y_min && a.x <= x_max && a.y <= y_max))
+                    {
+                        p.val = onOff ? 1 : 0;
+                    }
+
+
+                }
+                else
+                {
+                    var x_min = int.Parse(line_content[1].Split(',')[0]);
+                    var y_min = int.Parse(line_content[1].Split(',')[1]);
+
+                    var x_max = int.Parse(line_content[3].Split(',')[0]);
+                    var y_max = int.Parse(line_content[3].Split(',')[1]);
+
+                    foreach (var p in points.Where(a => a.x >= x_min && a.y >= y_min && a.x <= x_max && a.y <= y_max))
+                    {
+                        p.val = (p.val == 1) ? 0 : 1;
+                    }
+
+                }
+            }
+
+            return points.Sum(x => x.val);
+        }
+
+        static int PartTwo()
+        {
+
+            foreach (string line in File.ReadLines("./input"))
+            {
+                var line_content = line.Split(' ');
+                if (line_content[0] == "turn")
+                {
+                    var onOff = line_content[1] == "on";
+                    var x_min = int.Parse(line_content[2].Split(',')[0]);
+                    var y_min = int.Parse(line_content[2].Split(',')[1]);
+
+                    var x_max = int.Parse(line_content[4].Split(',')[0]);
+                    var y_max = int.Parse(line_content[4].Split(',')[1]);
+
+                    foreach (var p in points.Where(a => a.x >= x_min && a.y >= y_min && a.x <= x_max && a.y <= y_max))
+                    {
+                        p.val += onOff ? 1 : (p.val > 0 ? -1 : 0);
+                    }
+
+
+                }
+                else
+                {
+                    var x_min = int.Parse(line_content[1].Split(',')[0]);
+                    var y_min = int.Parse(line_content[1].Split(',')[1]);
+
+                    var x_max = int.Parse(line_content[3].Split(',')[0]);
+                    var y_max = int.Parse(line_content[3].Split(',')[1]);
+
+                    foreach (var p in points.Where(a => a.x >= x_min && a.y >= y_min && a.x <= x_max && a.y <= y_max))
+                    {
+                        p.val += 2;
+                    }
+
+                }
+            }
+
+            return points.Sum(x => x.val);
+        }
+
+        static void Main(string[] args)
+        {
+            /* Generate board */
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    points.Add(new Point(i, j));
+                }
+
+            }
+
+            Console.WriteLine("Part 1: " + PartOne());
+
+            foreach (var p in points)
+            {
+                p.val = 0;
+            }
+
+            Console.WriteLine("Part 1: " + PartTwo());
+        }
+    }
+}
---
+++ day7/day7/day7/Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Configuration;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using day7;
+
+namespace day7
+{
+
+    public class Wire
+    {
+
+        public static List<String> OPS = new List<String> { "NOT", "OR", "AND", "LSHIFT", "RSHIFT" };
+
+        public Wire(String input)
+        {
+            name = input;
+            value = 0;
+            literal = false;
+        }
+
+        public UInt16 GetValue()
+        {
+            if (calculated)
+            {
+                return value;
+            }
+
+            UInt16 n;
+            if (UInt16.TryParse(name, out n))
+            {
+                value = n;
+                calculated = true;
+                return n;
+            }
+
+            switch (op)
+            {
+                case "NOT":
+                    value = (UInt16)~lval.GetValue();
+                    break;
+                case "OR":
+                    value = (UInt16)(lval.GetValue() | rval.GetValue());
+                    break;
+                case "AND":
+                    value = (UInt16)(lval.GetValue() & rval.GetValue());
+                    break;
+                case "LSHIFT":
+                    value = (UInt16)(lval.GetValue() << rval.GetValue());
+                    break;
+                case "RSHIFT":
+                    value = (UInt16)(lval.GetValue() >> rval.GetValue());
+                    break;
+                default:
+                    value = lval.GetValue();
+                    break;
+            }
+            calculated = true;
+            return value;
+        }
+
+        public String name;
+        public UInt16 value { get; set; }
+        public Boolean calculated { get; set; }
+        public Boolean literal { get; set; }
+        public Wire lval { get; set; }
+        public Wire rval { get; set; }
+        public String op { get; set; }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var wires = new List<Wire>();
+            var OPS = new List<String> { "NOT", "OR", "AND", "LSHIFT", "RSHIFT" };
+
+
+            foreach (string line in File.ReadLines("./input"))
+            {
+                var w = line.Split(' ');
+                foreach (var wire in w.Where(x => x != "->" && !OPS.Contains(x)))
+                {
+                    if (wires.All(x => x.name != wire))
+                        wires.Add(new Wire(wire));
+                }
+
+                var res = wires.FirstOrDefault(x => x.name == w.Last());
+
+                res.op = w.FirstOrDefault(x => OPS.Contains(x));
+
+                switch (res.op)
+                {
+                    case "OR":
+                    case "AND":
+                        res.lval = wires.First(x => x.name == w[0]);
+                        res.rval = wires.First(x => x.name == w[2]);
+                        res.calculated = false;
+                        break;
+                    case "NOT":
+                        res.lval = wires.First(x => x.name == w[1]);
+                        res.calculated = false;
+                        break;
+                    case "LSHIFT":
+                        res.lval = wires.First(x => x.name == w[0]);
+                        res.rval = new Wire(w[2]) {value = UInt16.Parse(w[2]), calculated = true};
+                        res.calculated = false;
+                        break;
+                    case "RSHIFT":
+                        res.lval = wires.First(x => x.name == w[0]);
+                        res.rval = new Wire(w[2]) {value = UInt16.Parse(w[2]), calculated = true};
+                        res.calculated = false;
+                        break;
+                    default:
+                        /* Literal! */
+                        if (Regex.IsMatch(w[0], @"^[a-zA-Z]+$"))
+                        {
+                            res.lval = wires.First(x => x.name == w[0]);
+                            res.calculated = false;
+                        }
+                        else
+                        {
+                            res.value = UInt16.Parse(w[0]);
+                            res.literal = true;
+                            res.calculated = true;
+                        }
+
+                        break;
+                }
+
+            }
+
+            int a_val = wires.First(x => x.name == "a").GetValue();
+
+            Console.WriteLine("Part 1 => a: " + a_val);
+
+             /* Part 2: */
+
+            foreach (var w in wires.Where(x => x.calculated && !x.literal))
+            {
+                w.calculated = false;
+            }
+
+            wires.First(x => x.name == "b").value = (UInt16) a_val;
+
+            Console.WriteLine("Part 2 => a: " + wires.First(x => x.name == "a").GetValue());
+        }
+    }
+}
