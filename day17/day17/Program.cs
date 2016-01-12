01c30e4b5db21157c4b39ba9abb5f4412e8d6cce
Troels Krøgh <troels.kroegh@gmail.com>
2016-01-12 06:32:02 +0100

    day 17 part 1 and part 2

---
+++ day17/day17/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day17
+{
+    static class Program
+    {
+        private static List<int> input { get; set; } 
+        static void Main(string[] args)
+        {
+            input = new List<int>{11, 30, 47, 31, 32, 36, 3, 1, 5, 3, 32, 36, 15, 11, 46, 26, 28, 1, 19, 3};
+
+            var output = GetPowerSet(input).Where(x => x.Sum() == 150);
+            var minCount = output.Min(x => x.Count());
+
+
+            Console.WriteLine("Part 1: " + output.Count());
+            Console.WriteLine("Part 2: " + output.Count(x => minCount == x.Count()));
+        }
+
+        public static IEnumerable<IEnumerable<T>> GetPowerSet<T>(this IList<T> list)
+        {
+            return from m in Enumerable.Range(0, 1 << list.Count)
+                   select
+                       from i in Enumerable.Range(0, list.Count)
+                       where (m & (1 << i)) != 0
+                       select list[i];
+        }
+    }
+}
