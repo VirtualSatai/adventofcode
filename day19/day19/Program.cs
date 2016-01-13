7da6b0530dcd5955713ab26b8e024f496b0635b1
VirtualSatai <troels.kroegh@gmail.com>
2016-01-13 01:24:56 +0100

    day 19 part 1

---
+++ day19/day19/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("./input");
+            var rules = input.Where(x => x.Contains("=>"));
+            var startString = input.Last();
+
+            foreach (var rule in rules)
+            {
+                Console.WriteLine(rule);
+            }
+
+            Console.WriteLine(startString);
+
+            var posibleOutputs = new List<string>();
+            foreach (var r in rules)
+            {
+                var split = r.Split(Convert.ToChar(" "));
+                var ls = split.First();
+                var rs = split.Last();
+
+                for (int i = 0; i < startString.Length - ls.Length + 1; i++)
+                {
+                    if(startString.Substring(i, ls.Length) == ls)
+                        posibleOutputs.Add(startString.Remove(i, ls.Length).Insert(i, rs));
+                }
+            }
+
+            foreach (var po in posibleOutputs)
+            {
+                Console.WriteLine(po);
+            }
+
+
+            Console.WriteLine(posibleOutputs.Distinct().Count());
+
+            Console.WriteLine(posibleOutputs.Count());
+        }
+    }
+}
