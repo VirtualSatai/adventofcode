030c02f5ace559fee7f91911b1db6a3e16386bef
Troels Krøgh <troels.kroegh@gmail.com>
2016-01-13 02:18:42 +0100

    day 19 part 2

--- day19/day19/Program.cs
+++ day19/day19/Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace day19
@@ -21,8 +23,13 @@ namespace day19
                 Console.WriteLine(rule);
             }
 
-            Console.WriteLine(startString);
+            //Console.WriteLine(PartOne(rules, startString));
+            
+            PartTwo(rules, startString);
+        }
 
+        static int PartOne(IEnumerable<string> rules, string startString)
+        {
             var posibleOutputs = new List<string>();
             foreach (var r in rules)
             {
@@ -32,20 +39,36 @@ namespace day19
 
                 for (int i = 0; i < startString.Length - ls.Length + 1; i++)
                 {
-                    if(startString.Substring(i, ls.Length) == ls)
+                    if (startString.Substring(i, ls.Length) == ls)
                         posibleOutputs.Add(startString.Remove(i, ls.Length).Insert(i, rs));
                 }
             }
 
-            foreach (var po in posibleOutputs)
+            return posibleOutputs.Distinct().Count();
+        }
+
+        static void PartTwo(IEnumerable<string> rules, string endString)
+        {
+            var count = 0;
+            // Greedy replace the each occurance, no garrentee it's the best way. 
+            while (endString != "e")
             {
-                Console.WriteLine(po);
+                foreach (var r in rules)
+                {
+                    var split = r.Split(Convert.ToChar(" "));
+                    var ls = split.First();
+                    var rs = split.Last();
+
+                    if (endString.Contains(rs))
+                    {
+                        var regex = new Regex(rs);
+                        endString = regex.Replace(endString, ls, 1);
+                        count++;
+                    }
+                }
             }
 
-
-            Console.WriteLine(posibleOutputs.Distinct().Count());
-
-            Console.WriteLine(posibleOutputs.Count());
+            Console.WriteLine(count);
         }
     }
 }
