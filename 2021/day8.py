e1ed723c7fa1dd5eb0f2b90b6c6b44b9f9e4b85f
Troels Beck Krøgh <tbk@netcompany.com>
2021-12-09 14:27:57 +0100

    2021: Day 8 final

--- 2021/day8.py
+++ 2021/day8.py
@@ -32,21 +32,28 @@ def part1():
 
 
 def part2():
+    sum_all = 0
     for all_codes, answer_codes in inp:
         d_one = [c for c in all_codes if len(c) == 2][0]
         d_four = [c for c in all_codes if len(c) == 4][0]
         d_seven = [c for c in all_codes if len(c) == 3][0]
-        d_eights = [c for c in all_codes if len(c) == 7][0]
+        d_eight = [c for c in all_codes if len(c) == 7][0]
 
         d_zero_six_nine = [c for c in all_codes if len(c) == 6]
         d_six = [c for c in d_zero_six_nine if len([value for value in d_one if value in c]) == 1][0]
         d_nine = [c for c in d_zero_six_nine if len([value for value in d_four if value in c]) == 4][0]
         d_zero = [c for c in d_zero_six_nine if c != d_six and c != d_nine][0]
-        d_five = 'todo'
-        d_two = 'todo'
-        d_three = 'todo'
+        d_two_three_five = [c for c in all_codes if len(c) == 5]
+        d_five = [c for c in d_two_three_five if len([value for value in d_four if value in c]) == 3 and len([value for value in d_one if value in c]) == 1][0]
+        d_two = [c for c in d_two_three_five if len([value for value in d_four if value in c]) == 2 and len([value for value in d_one if value in c]) == 1][0]
+        d_three = [c for c in d_two_three_five if len([value for value in d_one if value in c]) == 2][0]
+        digets = ["".join(sorted(a)) for a in [d_zero, d_one, d_two, d_three, d_four, d_five, d_six, d_seven, d_eight, d_nine]]
+        answer = ''
+        for a in ["".join(sorted(a)) for a in answer_codes]:
+            answer += str(digets.index(a))
+        sum_all += int(answer)
 
-    print(f"Part 2: {2}")
+    print(f"Part 2: {sum_all}")
 
 part1()
 part2()
